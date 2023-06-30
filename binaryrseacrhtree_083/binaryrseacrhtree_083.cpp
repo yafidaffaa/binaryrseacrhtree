d2752159a799ad59d18647f682bac0cbd09a82ad
DimasCW <cdimas292@gmail.com>
2023-06-30 19:33:38 +0700

    membuat class search

--- binaryrseacrhtree_083/binaryrseacrhtree_083.cpp
+++ binaryrseacrhtree_083/binaryrseacrhtree_083.cpp
@@ -48,7 +48,12 @@ public:
 		}
 		else if (element > parent->info)//if the value in the data field of the new node is greater than that of the parent
 		{
-
+			parent->rightchild = newNode;///make the right child of the parent point to the new node
 		}
+	}
+
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+
 	}
 };
