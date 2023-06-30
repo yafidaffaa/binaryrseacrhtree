8b97be0f32d4fe9b08d310c6e2f6f895c0623432
DimasCW <cdimas292@gmail.com>
2023-06-30 19:27:13 +0700

    membuat iterasi parent == NULL

--- binaryrseacrhtree_083/binaryrseacrhtree_083.cpp
+++ binaryrseacrhtree_083/binaryrseacrhtree_083.cpp
@@ -27,6 +27,19 @@ public:
 
 	void insert(string element)//insert a node in the binary search tree
 	{
-	
+		Node* newNode = new Node(element, NULL, NULL);//Alocate memory for the new node
+		newNode->info = element;//Assign value to the data field of the new node point to NULL
+		newNode->leftchild = NULL;//make the left child of the new node point to NULL
+		newNode->rightchild = NULL;//make the right child of the new node point to null
+
+		Node* parent = NULL;
+		Node* currentNode = NULL;
+		search(element, parent, currentNode);//locate the node which will be the parent of the node to be interested
+
+		if (parent == NULL)//if the parent is NULL(tree is empty)
+		{
+			ROOT = newNode;//mark the new node as ROOT
+			return;//exit
+		}
 	}
 };
