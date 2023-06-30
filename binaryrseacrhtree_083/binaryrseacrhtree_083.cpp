5156e18ffac9b9961d1ca9d6883f018321e48288
DimasCW <cdimas292@gmail.com>
2023-06-30 19:37:54 +0700

    membuat method inorder

--- binaryrseacrhtree_083/binaryrseacrhtree_083.cpp
+++ binaryrseacrhtree_083/binaryrseacrhtree_083.cpp
@@ -62,6 +62,12 @@ public:
 			parent = currentNode;
 			if (element < currentNode->info)
 				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
 		}
+	}
+
+	void inorder(Node* ptr) {
+
 	}
 };
