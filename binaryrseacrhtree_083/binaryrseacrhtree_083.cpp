72645ca0a3afd9219f20f1c1dad236421361ecd8
DimasCW <cdimas292@gmail.com>
2023-06-30 19:35:56 +0700

    membuat perulangan denga  para meter (currentNode != NULL)&&(currentNode->info!=element)

--- binaryrseacrhtree_083/binaryrseacrhtree_083.cpp
+++ binaryrseacrhtree_083/binaryrseacrhtree_083.cpp
@@ -54,6 +54,12 @@ public:
 
 	void search(string element, Node*& parent, Node*& currentNode)
 	{
+		//this function searches the currentNode of the specified Node as well as the current Node of its parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL)&&(currentNode->info!=element))
+		{
 
+		}
 	}
 };
